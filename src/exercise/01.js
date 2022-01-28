87ae6d7d53cc85b4f0a69322be2e7781eaa6c22e
Daniel <daniel.robles-ext@dominion-global.com>
2022-01-28 09:57:55 -0600

    2nd lecture

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,27 +3,59 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting({initialName}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  // const name = ''
+  const [name, setName] = React.useState(initialName);
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" placeholder="John Doe" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
   )
 }
 
+// // To manage state from multiple inputs I used the tutorial
+// // https://www.pluralsight.com/guides/handling-multiple-inputs-with-single-onchange-handler-react
+
+// function Greeting({initialPerson}) {
+//   // const name = ''
+//   const [person, setPerson] = React.useState({firstName: initialPerson.firstName, lastName: initialPerson.lastName});
+
+//   function handleChange(event) {
+//     const value = event.target.value;
+//     setPerson({
+//       ...person,
+//    // Square brackets indicate "dynamic key"
+//       [event.target.name]: value
+//     });
+//   }
+
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input onChange={handleChange} name="firstName" value={person.firstName} placeholder="John"/>
+//         <label htmlFor="name">Last name: </label>
+//         <input onChange={handleChange} name="lastName" value={person.lastName} placeholder="Doe"/>
+//       </form>
+//       {person ? <strong>Hello {person.firstName} {person.lastName}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+
 function App() {
-  return <Greeting />
+  return <Greeting initialName='Your name here' />
+  // return <Greeting initialPerson='Your name here' />
 }
 
 export default App
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,15 +3,31 @@
 
 import * as React from 'react'
 
+// Custom hooks is just a fancy name to create code to reuse and be called just like callbacks in JS
+// (always with the word "use" before)
+// We also create generic parameters (key, defaultValue) to use later in the callback
+// 🐨 initialize the state to the value from localStorage
+// 💰 window.localStorage.getItem('name') ?? defaultValue
+
+// 🐨 Here's where you'll use `React.useEffect`.
+// The callback should set the `name` in localStorage.
+// 💰 window.localStorage.setItem('name', name)
+
+function useLocalStorageState(key, defaultValue='') {
+  // By including the initial value in a function, we tell useState to load only when an initial value is absent
+  // We set the function to access the localStorage variable of our choice by adding "key" and use "state" to refer an initial value
+  const [state, setState] = React.useState(() => {return window.localStorage.getItem(key) ?? defaultValue})
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  },
+  [key, state]
+  )
+  return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
-
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
-
+  const [name, setName] = useLocalStorageState(initialName)
   function handleChange(event) {
     setName(event.target.value)
   }
